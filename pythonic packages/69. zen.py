4aa02f47bf15edf374785b8bc497f84f65506ffe
Yuri Lamihov <48186454+lamihov@users.noreply.github.com>
2019-11-04 23:07:10 +0400

    Create 69. zen.py

---
+++ pythonic packages/69. zen.py
@@ -0,0 +1,24 @@
+'''
+Чтобы открыть «Дзен языка Python», используйте команду import this.
+
+1. Beautiful is better than ugly (Лучше красиво, чем некрасиво)
+2. Explicit is better than implicit (Лучше явно, чем неявно)
+3. Simple is better than complex (Простой код лучше, чем сложный)
+4. Complex is better than complicated (Сложный код лучше, чем запутанный)
+5. Flat is better than nested (Плоский код лучше, чем вложенный)
+6. Sparse is better than dense (Разреженный код лучше, чем плотный)
+7. Readability counts (Читаемостью нельзя пренебрегать)
+8. Special cases aren't special enough to break the rules (Особые случаи не настолько особенные, чтобы нарушать правила)
+9. Although practicality beats purity (При этом практичность важнее безупречности)
+10. Errors should never pass silently (Ошибки никогда не должны замалчиваться)
+11. Unless explicitly silenced (Замалчивать можно только явно)
+12. In the face of ambiguity, refuse the temptation to guess (Встретив двусмысленность, отбрось искушение угадать)
+13. There should be one-and preferably only one-obvious way to do it (Должен существовать один - и, желательно, только один - очевидный способ сделать что-либо)
+14. Although that way may not be obvious at first unless you're Dutch (Он будет очевиден сразу, только если вы голландец)
+15. Now is better than never (Сейчас лучше, чем никогда)
+16. Although never is often better than *right* now (Хотя никогда зачастую лучше, чем прямо сейчас)
+17. If the implementation is hard to explain, it's a bad idea (Если реализацию сложно объяснить - идея плохая)
+18. f the implementation is easy to explain, it may be a good idea (Если реализацию легко объяснить - идея, возможно, хорошая)
+19. Namespaces are one honking great idea-let's do more of those! (Пространства имен отличная штука! Давайте использовать их как можно больше!)
+'''
+import this
